69636cfb9765b4ea86dabf167bf9a85934552ded
SeungJin Kim <seungjinkim@SeungJins-MBP.attlocal.net>
2020-06-27 14:57:18 -0700

    Create function to create document in db

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,17 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+// const admin = require('firebase-admin');
 
-admin.initializeApp();
+// admin.initializeApp();
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fitcircle-97015.firebaseio.com"
+});
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -20,4 +30,23 @@ exports.getScreams = functions.https.onRequest((req, res) => {
       return res.json(screams);
     })
     .catch(err => console.error(err));
-})
+});
+
+exports.createScream = functions.https.onRequest((req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin.firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully`});
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'something went wrong'});
+      console.error(err);
+    });
+});
