8edfd983cee6e98df72d9b701249f1365963d921
SeungJin Kim <seungjinkim@SeungJins-MBP.attlocal.net>
2020-06-28 19:33:42 -0700

    Add express and refactor functions to express routes

--- functions/index.js
+++ functions/index.js
@@ -1,42 +1,31 @@
 const functions = require('firebase-functions');
+
 // const admin = require('firebase-admin');
-
 // admin.initializeApp();
-
 var admin = require("firebase-admin");
-
-var serviceAccount = require("../serviceAccountKey.json");
-
+var serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fitcircle-97015.firebaseio.com"
 });
 
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello World!");
-});
 
-exports.getScreams = functions.https.onRequest((req, res) => {
+app.get('/screams', (req, res) => {
   admin.firestore().collection('screams').get()
-    .then(data => {
-      let screams = [];
-      data.forEach(doc => {
-        screams.push(doc.data());
-      });
-      return res.json(screams);
-    })
-    .catch(err => console.error(err));
-});
+  .then(data => {
+    let screams = [];
+    data.forEach(doc => {
+      screams.push(doc.data());
+    });
+    return res.json(screams);
+  })
+  .catch(err => console.error(err));
+})
 
-exports.createScream = functions.https.onRequest((req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(400).json({ error: "Method not allowed"});
-  }
-  
+app.post('/scream', (req, res) => {  
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
@@ -54,3 +43,6 @@ exports.createScream = functions.https.onRequest((req, res) => {
       console.error(err);
     });
 });
+
+
+exports.api = functions.https.onRequest(app);
