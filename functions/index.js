15dc5fb0b800ef0bc0d7ca4ce7d2c63fae1f7303
SeungJin Kim <seungjinkim@SeungJins-MBP.attlocal.net>
2020-06-29 18:29:18 -0700

    Add sign up route

--- functions/index.js
+++ functions/index.js
@@ -59,4 +59,28 @@ app.post("/scream", (req, res) => {
     });
 });
 
+
+// Signup route
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  // TODO: validate data
+
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      console.log("data here", data);
+      return res.status(201).json({ message: `user ${data.user.uid} signed up successfully`});
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+});
+
+
 exports.api = functions.https.onRequest(app);
